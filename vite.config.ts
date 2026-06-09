ae83dd963b28ff6e3b883156eaa0e5068fcbab1d
XtreeE_Rungis <felipe.penagos>
2026-06-09 16:02:18 +0200

    404 error test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: './',
+      base: '/toolpathviz-pro/',
       server: {
         port: 3000,
         host: '0.0.0.0',
